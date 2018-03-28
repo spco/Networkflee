a1e9917ac93076cbec3eff50f3143c15b27f354c
Samuel Cox <sc676@leicester.ac.uk>
2018-03-28 15:25:09 +0100

    Fix bug in reuse of Router. First attempt at merging algorithm.

--- test.py
+++ test.py
@@ -32,10 +32,10 @@ def read_in_nodes(filename):
 
 
 def generate_all_cz_to_camp_routes(cz_list, camp_list):
-    router = Router("car")  # Initialise it
     list_of_routes = []
     for cz in cz_list:
         for camp in camp_list:
+            router = Router("car")  # Initialise it
             print(cz, camp)
             start = router.data.findNode(float(cz[0]), float(cz[1]))  # Find start and end nodes
             end = router.data.findNode(float(camp[0]), float(camp[1]))
@@ -48,10 +48,11 @@ def generate_all_cz_to_camp_routes(cz_list, camp_list):
                 routeLatLons.insert(0, cz)
                 routeLatLons.append(camp)
                 list_of_routes.append(routeLatLons)
-                print(len(routeLatLons))
+                print('here', len(routeLatLons))
             elif status == 'gave_up':
                 print('skipping as too long')
             else:
+                print(status)
                 assert False
 
     return(list_of_routes)
@@ -69,12 +70,28 @@ def extract_nodes(list_of_routes):
     return list_of_unique_nodes
 
 
+def merge_routes(list_of_routes):
+    route_a = list_of_routes[0]
+    route_b = list_of_routes[1]
+    this_shared_route = shared_route(route_a, route_b)
+    list_of_merged_routes = []
+    return list_of_merged_routes
+
+
+def shared_route(route_a, route_b):
+    this_shared_route = [element for element in route_a if element in route_b]
+    print(this_shared_route)
+    return this_shared_route
+
+
 if __name__ == "__main__":
     cz_list, camp_list = read_in_nodes('/home/s/sc676/Desktop/locations_sharing.csv')
     print('CZs: ', cz_list)
     print('camps: ', camp_list)
     list_of_routes = generate_all_cz_to_camp_routes(cz_list, camp_list)
-    print(list_of_routes)
+    print(list_of_routes[0])
+    print(list_of_routes[1])
     print(len(list_of_routes))
+    list_of_merged_routes = merge_routes(list_of_routes)
     list_of_nodes = extract_nodes(list_of_routes)
     print(list_of_nodes)
